bbd817faaf07805eb1a1b9c73e35fc9018ef3688
Fernando Lopez <frlopez88@icloud.com>
2025-05-06 20:33:36 -0600

    last commit

--- app.js
+++ app.js
@@ -1,17 +1,55 @@
-let products = [
-    {
-        name: "MacBook Pro 15",
-        price : 500,
-        year : 2019
-    }, 
-    {
-        name : "Iphone 16",
-        price : 1000,
-        year : 2025
-    },
-    {
-        name : "Ipad",
-        price : 700,
-        year : 2020
+let products = [];
+
+let token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh0bXlkemJhZmR4enVsdXhla3lpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1ODE4NTcsImV4cCI6MjA2MjE1Nzg1N30.af0ve3oxUYsM5XP6nUav00YO4CF03YCnXnXEEQBXDAo`
+
+async function getProducts() {
+
+    let response = await fetch("https://xtmydzbafdxzuluxekyi.supabase.co/rest/v1/products", {
+        method: "GET",
+        headers: {
+            'apikey': token,
+            'Authorization': token,
+            'Content-Type': "application/json"
+        }
+    })
+
+    let data = await response.json()
+    products = data
+
+    renderTable()
+    renderCards()
+}
+
+
+
+function renderTable() {
+
+    let dataTable = document.getElementById("tableDefinition")
+
+    for (let i = 0; i < products.length; i++) {
+
+        dataTable.innerHTML += `<tr>  
+    <td class="px-1 py-1 border-b text-left"> ${products[i].product_name} </td>
+    <td class="px-1 py-1 border-b text-center"> ${products[i].price}  </td>
+    <td class="px-1 py-1 border-b text-center"> ${products[i].year} </td>
+</tr>`
+
     }
-]
+}
+
+
+function renderCards (){
+    let cards = document.getElementById("cardContainer")
+
+    for (let i = 0; i < products.length; i++) {
+    
+        cards.innerHTML += `<div class="bg-white p-4 rounded shadow">
+                  <h2 class="text-xl text-black font-semibold">${products[i].product_name}</h2>
+                  <p class="text-gray-600">${products[i].price}</p>
+                  <p class="text-gray-600">${products[i].year}</p>
+                </div>`
+    
+    }
+}
+
+
