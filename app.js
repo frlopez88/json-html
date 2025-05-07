7c0efb16a5e93708e39b2c3d4bae00207bb172c4
Fernando Lopez <frlopez88@icloud.com>
2025-05-06 18:31:01 -0600

    Product Creation

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 let products = [
-    
+    {
+        name: "MacBook Pro 15",
+        price : 500,
+        year : 2019
+    }, 
+    {
+        name : "Iphone 16",
+        price : 1000,
+        year : 2025
+    },
+    {
+        name : "Ipad",
+        price : 700,
+        year : 2020
+    }
 ]
